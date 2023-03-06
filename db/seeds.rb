d80adc0e77721783ce41d2887318c073bb3f221d
Joanne Imbisi <evizengwa@yahoo.com>
2023-03-06 13:09:25 -0800

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# coffees = [
+# {
+#     name: Faker::Coffee.blend_name, 
+#     country: Faker::Coffee.origin,
+#     price: Faker::Number.between(from: 1, to: 10)
+# },
+# {
+#     name: Faker::Coffee.blend_name, 
+#     country: Faker::Coffee.origin,
+#     price: Faker::Number.between(from: 1, to: 10)
+# },
+# {
+#     name: Faker::Coffee.blend_name, 
+#     country: Faker::Coffee.origin,
+#     price: Faker::Number.between(from: 1, to: 10)
+# },
+# {
+#     name: Faker::Coffee.blend_name, 
+#     country: Faker::Coffee.origin,
+#     price: Faker::Number.between(from: 1, to: 10)
+# },
+# {
+#     name: Faker::Coffee.blend_name, 
+#     country: Faker::Coffee.origin,
+#     price: Faker::Number.between(from: 1, to: 10)
+# },
+# {
+#     name: Faker::Coffee.blend_name, 
+#     country: Faker::Coffee.origin,
+#     price: Faker::Number.between(from: 1, to: 10)
+# }
+# ]
+
+
+20.times do
+    Coffee.create!(name: Faker::Coffee.blend_name, country: Faker::Coffee.origin,
+                    price: Faker::Number.between(from: 1, to: 10))
+end
+
+
