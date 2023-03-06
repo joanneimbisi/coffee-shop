c826cc875d23dbcb17cb0f511deb4adfc97744a7
Joanne Imbisi <evizengwa@yahoo.com>
2023-03-06 13:08:36 -0800

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from './Signup';
+import Coffees from './Coffees';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <Routes>
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/coffees' element={<Coffees />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
